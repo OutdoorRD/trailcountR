5082186f0509fc0a18261e3a0e84571d2dddc8e9
Lesley <iamthelm@gmail.com>
2020-09-01 13:18:11 -0700

    edited the docstring to make a better toy dataframe for the example

--- R/insert_zero_counts.R
+++ R/insert_zero_counts.R
@@ -1,3 +1,17 @@
+#' Replaces NA counts with zeros.
+#'
+#' @param df A dataframe of IR counts.
+#'
+#' @return A dataframe of IR data with missing values filled in.
+#' @export
+#'
+#' @examples
+#' df <- tibble::tibble(
+#' PlacementID = c(102, 103, 104, 105),
+#' date = c("19-06-04", "19-06-04", "19-07-04", "19-07-06"),
+#' time = c("14:00", "14:00", "13:01", "12:02"),
+#' count = c(15, NA, 12, NA))
+#' insert_zero_counts(df)
 insert_zero_counts <- function(df) {
 
 	## get the rows with NAs for the count
