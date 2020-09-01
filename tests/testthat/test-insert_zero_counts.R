c541eea0cd653b8d795e78f59ba220885fe8e762
Lesley <iamthelm@gmail.com>
2020-09-01 13:18:57 -0700

    added a unit test

--- tests/testthat/test-insert_zero_counts.R
+++ tests/testthat/test-insert_zero_counts.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Zero counts are added correctly", {
+	df_missing <- df_complete <- tibble::tibble(
+		PlacementID = c(102, 103, 104, 105),
+		date = c("19-06-04", "19-06-04", "19-07-04", "19-07-06"),
+		time = c("14:00", "14:00", "13:01", "12:02"),
+		count = c(15, NA, 12, NA))
+
+	df_complete <- tibble::tibble(
+		PlacementID = c(102, 103, 104, 105),
+		date = c("19-06-04", "19-06-04", "19-07-04", "19-07-06"),
+		time = c("14:00", "14:00", "13:01", "12:02"),
+		count = c(15, 0, 12, 0))
+
+  expect_equal(insert_zero_counts(df_missing), df_complete)
 })
