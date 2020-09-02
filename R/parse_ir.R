31a98eb7be8f6878e8f1b748f8642a55bf0bf189
Lesley <iamthelm@gmail.com>
2020-09-01 21:07:45 -0700

    added the raw ir parsing fxn

---
+++ R/parse_ir.R
@@ -0,0 +1,22 @@
+#' Parses a raw IR txt file into a csv
+#'
+#' @param ir_file Name of raw IR file (do not include the path to file).
+#' @param ir_dir Path to the directory containing raw IR file.
+#' @param parsed_dir Path to directory where parsed csv should be written.
+#' @param dump_dir Path to directory where potentially corrupt files
+#' should be written
+#'
+#' @return NULL
+#'
+#' @export
+#'
+parse_ir <- function(ir_file, ir_dir, parsed_dir, dump_dir) {
+
+	reticulate::source_python("src/parse_ir.py")
+	ir <- parse_ir_to_csv(ir_file = ir_file,
+					ir_dir = ir_dir,
+					parsed_dir = parsed_dir,
+					dump_dir = dump_dir)
+
+	return(NULL)
+}
