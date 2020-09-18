89b5d5a5e381fc490499d0bd7d4063111f49f9fa
Lesley <iamthelm@gmail.com>
2020-09-18 07:12:07 -0700

    removed the sorting of the date step as it pushed an NAs to the end of the date frame

--- R/make_complete_timeseries.R
+++ R/make_complete_timeseries.R
@@ -22,24 +22,20 @@ make_complete_timeseries <- function(df) {
 	# ensure dates are a date object
 	df$date <- lubridate::ymd(df$date)
 
-	# ensure df is sorted by date
-	sorted_df <- df %>%
-		dplyr::arrange(date)
-
 	# Sometimes a trafx file prints an incorrect date-time
 	# at the time of download. Have to check for that last date is after first
 	# date in timeseries before creating the series of days of operation,
 	# which uses it for an end date.
 
-	start_date <- sorted_df$date[1]
-	stop_date <- sorted_df$date[nrow(sorted_df)]
+	start_date <- df$date[1]
+	stop_date <- df$date[nrow(df)]
 
 	if (stop_date >= start_date) {
 		dayseries <- seq(from = start_date, to = stop_date, by = "days")
 
 	} else {
-		dayseries <- seq(from = start_date, to = sorted_df[nrow(sorted_df) - 1,"date"]$date, by = "days")
-		print(sorted_df)
+		dayseries <- seq(from = start_date, to = df[nrow(df) - 1,"date"]$date, by = "days")
+		print(df)
 		print("WARNING: device improperly recorded download time,
           so final date of operation is taken from the last positive count instead.")
 	}
@@ -48,13 +44,13 @@ make_complete_timeseries <- function(df) {
 	dfdays <- tibble::tibble(date = dayseries)
 
 	# join the full timeseries to the data
-	newdat <- dplyr::full_join(dfdays, sorted_df, by = "date")
+	newdat <- dplyr::full_join(dfdays, df, by = "date")
 
 	## get the rows with NAs for the placementId
 	zeros <- which(is.na(newdat$PlacementID))
 
 	# add a placement id for the new inserted rows
-	newdat$PlacementID[zeros] <- sorted_df$PlacementID[1]
+	newdat$PlacementID[zeros] <- df$PlacementID[1]
 
 	return(newdat)
 
